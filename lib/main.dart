57f7d1a1c77121afeed348e98b5274daa3a4cc17
Admin <amee.saurabhinfosys@gmail.com>
2024-03-11 20:23:16 +0530

    11/3/24 8:23

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        fontFamily: 'Mo',
+        fontFamily: "Mo",
 
       // colorScheme: ColorScheme.dark(),
         useMaterial3: true,
